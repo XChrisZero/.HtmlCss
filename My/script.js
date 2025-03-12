e8400f2f938ad784dac372c8e83fec44c4a2c769
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2025-03-12 20:36:31 -0300

    A2U2

---
+++ My/script.js
@@ -0,0 +1,49 @@
+function horaAtual()
+{
+    document.getElementById('date').innerHTML = Date()
+}
+
+function mudarCor(  )
+{
+    document.getElementById('primeiro').style.backgroundColor='red'
+    document.getElementById('segundo').style.backgroundColor='red'
+    document.getElementById('terceiro').style.backgroundColor='red'
+}
+
+function voltarCor(  )
+{
+    document.getElementById('primeiro').style.backgroundColor=''
+    document.getElementById('segundo').style.backgroundColor=''
+    document.getElementById('terceiro').style.backgroundColor=''
+}
+
+function tamanhoLetra()
+{
+    
+    document.getElementById('primeiro').style.fontSize  ='50'
+    document.getElementById('segundo').style.fontSize  ='50'
+    document.getElementById('terceiro').style.fontSize  ='50'
+
+}
+
+function passarEmCima()
+{
+    document.getElementById('primeiro').style.backgroundColor='black'
+    document.getElementById('segundo').style.backgroundColor='black'
+    document.getElementById('terceiro').style.backgroundColor='black'
+    
+}
+
+function tirarMouse()
+{
+    document.getElementById('primeiro').style.backgroundColor=''   
+    document.getElementById('segundo').style.backgroundColor=''
+    document.getElementById('terceiro').style.backgroundColor=''
+}
+
+function alerta1()
+{
+
+}
+
+alert(id="primeiro");
